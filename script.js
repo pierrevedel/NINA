3f040545a2ffd797f499750e77127ac9078c6284
pierrevedel <pierre_vedel@hotmail.com>
2024-12-06 13:52:28 +0100

    CdC initial
    interface recu+ image au hasard + phrase au hasard

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Texte préparé
-const preparedText = " - Ceci est un ajout préparé";
+// Tableau de textes préparés
+const preparedTexts = [
+    "mon père me disait",
+    "si j'avais de l'argent je ferais",
+    "quand je ferme les yeux, je vois"
+];
 
 // Gestion du bouton
 document.getElementById("update-button").addEventListener("click", () => {
@@ -9,10 +13,13 @@ document.getElementById("update-button").addEventListener("click", () => {
         return;
     }
 
+    // Sélection d'un texte préparé au hasard
+    const preparedText = preparedTexts[Math.floor(Math.random() * preparedTexts.length)];
+
     // Mettre à jour le texte sur l'image
-    document.getElementById("dynamic-text").innerText = userInput + preparedText;
+    document.getElementById("dynamic-text").innerText = `${preparedText} ${userInput}`;
 
     // Mettre à jour l'image
-    const randomIndex = Math.floor(Math.random() * 10) + 1; // Choisir un nombre entre 1 et 10
+    const randomIndex = Math.floor(Math.random() * 5) + 1; // Choisir un nombre entre 1 et 10
     document.getElementById("dynamic-image").src = `images/img${randomIndex}.jpg`;
 });
